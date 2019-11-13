ea88137dd82df5701c06f03014435a3b24be1761
bestony <=best.tony@foxmail.com>
2019-11-13 15:21:16 +0800

    feat: add travis package

---
+++ .github/release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -rf .zip-files
+mkdir .zip-files
+
+echo "Search Directory to Package"
+
+for dir in `find * -mindepth 1 -maxdepth 2 -type d`; do
+    zip -r .zip-files/${dir##*/}.zip $dir
+done
