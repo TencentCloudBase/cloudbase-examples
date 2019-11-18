f1bfcc23f71ee3ffa8a66b18621ff4e74cae878e
bestony <best.tony@foxmail.com>
2019-11-19 00:30:04 +0800

    fix: package more directory than design

--- .github/release.sh
+++ .github/release.sh
@@ -5,6 +5,6 @@ mkdir .zip-files
 
 echo "Search Directory to Package"
 
-for dir in `find * -mindepth 1 -maxdepth 2 -type d`; do
+for dir in `find * -mindepth 1 -maxdepth 1 -type d`; do
     zip -r .zip-files/${dir##*/}.zip $dir
 done
