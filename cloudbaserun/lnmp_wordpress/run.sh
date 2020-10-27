238b66aef8f23a1933d8123a18378098af061cb4
Weijia Wang <starkwang@126.com>
2020-10-27 16:54:56 +0800

    rm wordpress

--- cloudbaserun/lnmp_wordpress/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-/etc/init.d/mysqld start
-
-echo "hello world" && /usr/local/mysql/bin/mysql -uroot -e "create user 'tcb'@'localhost' identified by 'tcb'" && \
-     /usr/local/mysql/bin/mysql -uroot -e "create database wordpress" && /usr/local/mysql/bin/mysql -uroot -e "grant all privileges on wordpress.* to 'tcb'@'localhost' identified by 'tcb'" && /usr/local/nginx/sbin/nginx  && /usr/local/php/sbin/php-fpm -c /usr/local/php/etc/php-fpm.conf
