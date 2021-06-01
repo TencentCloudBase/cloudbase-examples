0639870326a39190f8170df15c164f1647a62002
aa1173522029 <77425702+aa1173522029@users.noreply.github.com>
2021-06-01 17:45:09 +0800

    Update main.py

--- cloudbaserun/python/main.py
+++ cloudbaserun/python/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 1+1+1+1
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8080)
