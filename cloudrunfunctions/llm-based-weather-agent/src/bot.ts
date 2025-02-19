f476a02ccdf907ec2fc235b69cb4df44bdf87c8e
Areo-Joe <1047726015@qq.com>
2025-02-19 15:26:03 +0800

    fix: fix param using

--- cloudrunfunctions/llm-based-weather-agent/src/bot.ts
+++ cloudrunfunctions/llm-based-weather-agent/src/bot.ts
@@ -1,137 +1,147 @@
-import { TcbExtendedContext, ContextInjected } from '@cloudbase/functions-typings'
+import {
+  TcbExtendedContext,
+  ContextInjected,
+} from "@cloudbase/functions-typings";
 
 import {
   IBot,
   SendMessageInput,
-  GetChatRecordInput, GetChatRecordOutput,
-  GetRecommendQuestionsInput, GetRecommendQuestionsOutputChunk,
-  SendFeedbackInput, SendFeedbackOutput,
-  GetFeedbackInput, GetFeedbackOutput
- } from './aiagent-framework/type'
+  GetChatRecordInput,
+  GetChatRecordOutput,
+  GetRecommendQuestionsInput,
+  SendFeedbackInput,
+  SendFeedbackOutput,
+  GetFeedbackInput,
+  GetFeedbackOutput,
+} from "./aiagent-framework/type";
 
-import { getCloudbaseAi } from './cloudbase-ai'
+import { getCloudbaseAi } from "./cloudbase-ai";
 
 const WEATHERS = [
-  '晴空万里',
-  '乌云密布',
-  '狂风暴雨',
-  '风和日丽',
-  '电闪雷鸣',
-  '细雨绵绵',
-  '烈日炎炎',
-  '雪花飘飘',
-  '寒风凛冽',
-  '云淡风轻',
-]
+  "晴空万里",
+  "乌云密布",
+  "狂风暴雨",
+  "风和日丽",
+  "电闪雷鸣",
+  "细雨绵绵",
+  "烈日炎炎",
+  "雪花飘飘",
+  "寒风凛冽",
+  "云淡风轻",
+];
 
 const getRandomWeather = () => {
-  const index = Math.floor(Math.random() * WEATHERS.length)
-  return WEATHERS[index]
-}
+  const index = Math.floor(Math.random() * WEATHERS.length);
+  return WEATHERS[index];
+};
 
 export class MyBot extends IBot {
-  constructor(readonly context: ContextInjected<TcbExtendedContext>, readonly envId: string) {
-    super(context, envId)
+  constructor(
+    readonly context: ContextInjected<TcbExtendedContext>,
+    readonly envId: string
+  ) {
+    super(context, envId);
   }
 
-  async sendMessage(input: SendMessageInput): Promise<void> {
-    const ai = await getCloudbaseAi(this.envId)
+  async sendMessage({ msg, history = [] }: SendMessageInput): Promise<void> {
+    const ai = await getCloudbaseAi(this.envId);
     // 1. 定义获取天气的工具，详见 FunctionTool 类型
     const getWeatherTool = {
-      name: 'get_weather',
-      description: '返回某个城市的天气信息。调用示例：get_weather({city: \'北京\'})',
+      name: "get_weather",
+      description:
+        "返回某个城市的天气信息。调用示例：get_weather({city: '北京'})",
       fn: ({ city }) => `${city}的天气是：${getRandomWeather()}！！！`, // 在这定义工具执行的内容
-        parameters: {
-          type: 'object',
-          properties: {
-            city: {
-              type: 'string',
-              description: '要查询的城市',
-            },
+      parameters: {
+        type: "object",
+        properties: {
+          city: {
+            type: "string",
+            description: "要查询的城市",
           },
-          required: ['city'],
         },
-    }
+        required: ["city"],
+      },
+    };
 
     // 2. 注册我们刚定义好的工具
-    ai.registerFunctionTool(getWeatherTool)
-  
-    const model = ai.createModel('hunyuan-exp')
-  
+    ai.registerFunctionTool(getWeatherTool);
+
+    const model = ai.createModel("hunyuan-exp");
+
     const res = await model.streamText({
-      model: 'hunyuan-turbo',
+      model: "hunyuan-turbo",
       messages: [
         {
-          role: 'system',
+          role: "system",
           content:
-            '你是一个可以处理询问天气的机器人。用户可能会问你有关天气的问题，你可以调用工具函数进行查询。',
+            "你是一个可以处理询问天气的机器人。用户可能会问你有关天气的问题，你可以调用工具函数进行查询。",
         },
         ...((history as any) ?? []),
         {
-          role: 'user',
-          content: input.msg,
+          role: "user",
+          content: msg,
         },
       ],
       tools: [getWeatherTool],
-    })
-  
+    });
+
     for await (const chunk of res.dataStream) {
       this.sseSender.send({
-        content: chunk?.choices?.[0]?.delta?.content ?? '',
-        model: 'hunyuan-turbo',
-        finish_reason: chunk?.choices?.[0]?.finish_reason ?? '',
-      })
+        content: chunk?.choices?.[0]?.delta?.content ?? "",
+        model: "hunyuan-turbo",
+        finish_reason: chunk?.choices?.[0]?.finish_reason ?? "",
+      });
     }
 
-    this.sseSender.end()
+    this.sseSender.end();
   }
 
   async getChatRecord(input: GetChatRecordInput): Promise<GetChatRecordOutput> {
     const ret: GetChatRecordOutput = {
       recordList: [],
       total: 0,
-    }
-    return ret
+    };
+    return ret;
   }
 
   async recommendQuestions(input: GetRecommendQuestionsInput): Promise<void> {
-    const ai = await getCloudbaseAi(this.envId)
+    const ai = await getCloudbaseAi(this.envId);
 
-    const model = ai.createModel('hunyuan-exp')
+    const model = ai.createModel("hunyuan-exp");
     const res = await model.streamText({
-      model: 'hunyuan-turbo',
+      model: "hunyuan-turbo",
       messages: [
         {
-          role: 'system',
+          role: "system",
           content:
-            '你是一个可以处理询问天气的机器人。用户可能会问你有关天气的问题，你可以调用工具函数进行查询。',
+            "你是一个可以处理询问天气的机器人。用户可能会问你有关天气的问题，你可以调用工具函数进行查询。",
         },
         {
-          role: 'user',
-          content: '你会推荐我问你什么问题？举三个例子。',
+          role: "user",
+          content: "你会推荐我问你什么问题？举三个例子。",
         },
       ],
-    })
+    });
     for await (const chunk of res.dataStream) {
       this.sseSender.send({
-        content: chunk?.choices?.[0]?.delta?.content ?? '',
-        model: 'hunyuan-turbo',
-        finish_reason: chunk?.choices?.[0]?.finish_reason ?? '',
-      })
+        content: chunk?.choices?.[0]?.delta?.content ?? "",
+        model: "hunyuan-turbo",
+        finish_reason: chunk?.choices?.[0]?.finish_reason ?? "",
+      });
     }
-    this.sseSender.end()
+    this.sseSender.end();
   }
 
   async sendFeedback?(input: SendFeedbackInput): Promise<SendFeedbackOutput> {
-    return { status: 'success' }
+    return { status: "success" };
   }
 
   async getFeedback?(input: GetFeedbackInput): Promise<GetFeedbackOutput> {
     const ret: GetFeedbackOutput = {
       feedbackList: [],
       total: 0,
-    }
-  
-    return ret
+    };
+
+    return ret;
   }
 }
