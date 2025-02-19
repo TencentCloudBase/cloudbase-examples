1049be8a63f35353f786df4816ff8fec285ade58
Areo-Joe <1047726015@qq.com>
2025-02-19 15:25:29 +0800

    fix: complete methods route

--- cloudrunfunctions/llm-based-weather-agent/src/aiagent-framework/bot-runner.ts
+++ cloudrunfunctions/llm-based-weather-agent/src/aiagent-framework/bot-runner.ts
@@ -1,6 +1,6 @@
-import { IBot } from './type'
+import { IBot } from "./type";
 
-import { IntegrationResponse } from '@cloudbase/functions-typings'
+import { IntegrationResponse } from "@cloudbase/functions-typings";
 
 export function createIntegrationResponse(
   statusCode: number,
@@ -9,18 +9,18 @@ export function createIntegrationResponse(
   return {
     statusCode,
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: {
       message,
     },
-  }
+  };
 }
 
 export function parseApiName(url: string): string {
   const { pathname } = new URL(url);
 
-  const [_, botId, apiName] = pathname.split('/')
+  const [_, botId, apiName] = pathname.split("/");
 
   if (!(botId && apiName)) {
     return null;
@@ -30,39 +30,58 @@ export function parseApiName(url: string): string {
 }
 
 export class BotRunner {
+  private ROUTE_MAP = [
+    { methodName: "send-message", httpMethod: "POST", target: "sendMessage" },
+    {
+      methodName: "recommend-questions",
+      httpMethod: "POST",
+      target: "recommendQuestions",
+    },
+    {
+      methodName: "records",
+      httpMethod: "GET",
+      target: "getChatRecord",
+    },
+    {
+      methodName: "feedback",
+      httpMethod: "GET",
+      target: "getFeedback",
+    },
+    {
+      methodName: "feedback",
+      httpMethod: "POST",
+      target: "sendFeedback",
+    },
+  ];
+
   static async run(event, context, bot: IBot) {
-    return (new BotRunner(bot)).run(event, context)
+    return new BotRunner(bot).run(event, context);
   }
 
   constructor(readonly bot: IBot) {}
 
   async run(event, context) {
     if (!context.httpContext) {
-      return createIntegrationResponse(500, 'Invalid HTTP context')
+      return createIntegrationResponse(500, "Invalid HTTP context");
     }
 
-    const apiName = parseApiName(context.httpContext.url)
-    if (context.httpContext.httpMethod === 'POST' && apiName === 'send-message') {
-      return this.sendMessageFunc(event, context)
-    }
+    const apiName = parseApiName(context.httpContext.url);
 
-    return createIntegrationResponse(404, 'Invalid path')
-  }
+    const item = this.ROUTE_MAP.find(
+      (x) =>
+        x.httpMethod === context.httpContext.httpMethod &&
+        apiName === x.methodName
+    );
 
-  private async sendMessageFunc (event, context) {
-   const { msg, history = [] } = event
-   if (typeof context.extendedContext?.envId !== 'string') {
-     return createIntegrationResponse(400, 'invalid envId')
-   }
-
-   try {
-     await this.bot.sendMessage({msg, history})
-   } catch (e) {
-     return createIntegrationResponse(
-       400,
-       `Send message to Agent failed, detail: ${e}`
-     )
-   }
-   return ''
+    return item
+      ? this.bot[item.target](event, context)
+      : createIntegrationResponse(
+          404,
+          "Invalid path" +
+            JSON.stringify({
+              apiName,
+              httpMethod: context.httpContext.httpMethod,
+            })
+        );
   }
 }
