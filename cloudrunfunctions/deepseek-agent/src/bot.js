70ff2d38479ad0d07f2fc069820f673f87bb85b7
Areo-Joe <1047726015@qq.com>
2025-02-26 17:55:34 +0800

    fix: use openai sdk; update deps

--- cloudrunfunctions/deepseek-agent/src/bot.js
+++ cloudrunfunctions/deepseek-agent/src/bot.js
@@ -1,6 +1,5 @@
 const { BotCore } = require("@cloudbase/aiagent-framework");
-
-const { getCloudbaseAi } = require("./cloudbase-ai");
+const OpenAI = require("openai");
 
 /**
  * @typedef {import('@cloudbase/aiagent-framework').IBot} IBot
@@ -9,27 +8,36 @@ const { getCloudbaseAi } = require("./cloudbase-ai");
  * @implements {IBot}
  */
 class MyBot extends BotCore {
+  get apiKey() {
+    const accessToken = this.context?.extendedContext?.accessToken;
+    if (typeof accessToken !== "string") {
+      throw new Error("Invalid accessToken");
+    }
+
+    const apiKey = accessToken.replace("Bearer", "").trim();
+
+    return apiKey;
+  }
+
   async sendMessage({ msg }) {
-    const ai = await getCloudbaseAi(this.envId);
-
-    const model = ai.createModel("deepseek");
-
-    const res = await model.streamText({
-      model: "deepseek-r1",
-      messages: [
-        {
-          role: "user",
-          content: msg,
-        },
-      ],
+    const client = new OpenAI({
+      apiKey: this.apiKey,
+      baseURL: `https://${this.context.extendedContext.envId}.api.tcloudbasegateway.com/v1/ai/deepseek/v1`,
     });
 
-    for await (const data of res.dataStream) {
+    const stream = await client.chat.completions.create({
+      model: "deepseek-r1",
+      messages: [{ role: "user", content: msg }],
+      stream: true,
+    });
+
+    for await (const chunk of stream) {
       this.sseSender.send({
         data: {
-          ...data,
-          content: data?.choices?.[0]?.delta?.content || "",
-          reasoning_content: data?.choices?.[0]?.delta?.reasoning_content || "",
+          ...chunk,
+          content: chunk?.choices?.[0]?.delta?.content || "",
+          reasoning_content:
+            chunk?.choices?.[0]?.delta?.reasoning_content || "",
         },
       });
     }
