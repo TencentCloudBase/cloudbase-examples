2ea445d76e9674005c5cbbd557f4bcb9df76de9a
黄静远 <837317210@qq.com>
2025-03-21 23:06:29 +0800

    fix: fix to adapt to client

--- cloudrunfunctions/llm-based-weather-agent/src/bot.ts
+++ cloudrunfunctions/llm-based-weather-agent/src/bot.ts
@@ -12,9 +12,9 @@ import {
 } from "@cloudbase/aiagent-framework";
 
 import { getCloudbaseAi } from "./cloudbase-ai";
+const OpenAI = require("openai")
 
 const WEATHERS = [
-  "晴空万里",
   "乌云密布",
   "狂风暴雨",
   "风和日丽",
@@ -26,59 +26,105 @@ const WEATHERS = [
   "云淡风轻",
 ];
 
-const getRandomWeather = () => {
+const getRandomWeather = (city: string) => {
+  if (city === '北京') {
+    return '晴空万里'
+  }
   const index = Math.floor(Math.random() * WEATHERS.length);
   return WEATHERS[index];
 };
 
 export class MyBot extends BotCore implements IBot {
   async sendMessage({ msg, history }: SendMessageInput): Promise<void> {
-    const ai = await getCloudbaseAi(this.context.extendedContext?.envId!);
-    // 1. 定义获取天气的工具，详见 FunctionTool 类型
-    const getWeatherTool = {
-      name: "get_weather",
-      description:
-        "返回某个城市的天气信息。调用示例：get_weather({city: '北京'})",
-      fn: ({ city }: { city: string }) => `${city}的天气是：${getRandomWeather()}！！！`, // 在这定义工具执行的内容
-      parameters: {
-        type: "object",
-        properties: {
-          city: {
-            type: "string",
-            description: "要查询的城市",
+    const client = new OpenAI({
+      apiKey: this.apiKey,
+      baseURL: `https://${this.context.extendedContext.envId}.api.tcloudbasegateway.com/v1/ai/hunyuan-exp/v1`,
+    });
+    console.log('*****msg****', msg);
+
+    const tools = [{
+      "type": "function",
+      "function": {
+        "name": "get_weather",
+        "description": "返回某个城市的天气信息。调用示例：get_weather({city: '北京'})",
+        "parameters": {
+          "type": "object",
+          "properties": {
+            "city": {
+              "type": "string",
+              "description": "要查询的城市"
+            }
           },
+          "required": [
+            "city"
+          ],
+          "additionalProperties": false
         },
-        required: ["city"],
-      },
-    };
+        "strict": true
+      }
+    }]
 
-    // 2. 注册我们刚定义好的工具
-    ai.registerFunctionTool(getWeatherTool);
-
-    const model = ai.createModel("hunyuan-exp");
-
-    const res = await model.streamText({
+    const messages: any = [
+      { role: "user", content: msg }
+    ]
+    // 1. 将消息和工具定义发送给 AI
+    const completion = await client.chat.completions.create({
       model: "hunyuan-turbo",
-      messages: [
-        {
-          role: "system",
-          content:
-            "你是一个可以处理询问天气的机器人。用户可能会问你有关天气的问题，你可以调用工具函数进行查询。",
+      messages,
+      tools,
+      temperature: 0.3,
+      // stream: true,
+    })
+
+    console.log('completion', JSON.stringify(completion.choices[0].message));
+    if (completion.choices[0].message.content) {
+      // 回一次包给客户端
+      this.sseSender.send({
+        data: {
+          content: `\n\n${completion.choices[0].message.content ?? ""}\n\n`,
+          model: "hunyuan-turbo",
+          finish_reason: "",
+          role: 'assistant',
+          type: 'text',
         },
-        {
-          role: "user",
-          content: msg,
-        },
-      ],
-      tools: [getWeatherTool],
+      });
+    }
+    if (completion.choices[0].message.tool_calls) {
+      const toolCall = completion.choices[0].message.tool_calls[0]
+      console.log('toolCall', toolCall);
+      const args = JSON.parse(toolCall.function.arguments)
+      // 2. 开发者自定义获取天气结果逻辑
+      let result = toolCall.function.name === 'get_weather' ? getRandomWeather(args.city) : ''
+      console.log('result', result);
+
+      // 3. 将结果合并到messags中
+      messages.push(completion.choices[0].message)
+      messages.push({
+        role: "tool",
+        tool_call_id: toolCall.id,
+        content: result.toString()
+      });
+    }
+
+    // 将最终的消息数组发给AI进行总结
+    const completion2 = await client.chat.completions.create({
+      model: "hunyuan-turbo",
+      messages,
+      tools,
+      store: true,
+      stream: true
     });
 
-    for await (const chunk of res.dataStream) {
+
+    for await (const chunk of completion2) {
+      console.log('chunk', JSON.stringify(chunk?.choices?.[0]));
       this.sseSender.send({
         data: {
           content: chunk?.choices?.[0]?.delta?.content ?? "",
           model: "hunyuan-turbo",
           finish_reason: chunk?.choices?.[0]?.finish_reason ?? "",
+          role: 'assistant',
+          type: 'text',
         },
       });
     }
@@ -135,5 +181,15 @@ export class MyBot extends BotCore implements IBot {
     };
 
     return ret;
+  }
+
+  // 从上下文中获取云开发 accessToken
+  get apiKey() {
+    const accessToken = this.context?.extendedContext?.accessToken;
+    if (typeof accessToken !== "string") {
+      throw new Error("Invalid accessToken");
+    }
+
+    return accessToken.replace("Bearer", "").trim();
   }
 }
