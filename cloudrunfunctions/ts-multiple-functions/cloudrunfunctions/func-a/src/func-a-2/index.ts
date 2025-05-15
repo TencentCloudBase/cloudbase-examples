6e096f4c6864e50d055178dd63d56f33f3f0b92a
liuyanjie <lyj8888888888@gmail.com>
2025-05-15 21:20:47 +0800

    feat: use commonjs

--- cloudrunfunctions/ts-multiple-functions/cloudrunfunctions/func-a/src/func-a-2/index.ts
+++ cloudrunfunctions/ts-multiple-functions/cloudrunfunctions/func-a/src/func-a-2/index.ts
@@ -1,7 +1,10 @@
 import { TcbEventFunction } from '@cloudbase/functions-typings'
 
+import { x } from './lib'
+
 // GET no boyd
 export const main: TcbEventFunction = function(event, context) {
+  console.log('x', x)
   console.log('hello a-2')
   return 'a-2'
 }
--- cloudrunfunctions/ts-multiple-functions/cloudrunfunctions/func-b/src/index.ts
+++ cloudrunfunctions/ts-multiple-functions/cloudrunfunctions/func-b/src/index.ts
@@ -1,7 +1,10 @@
 import { TcbEventFunction } from '@cloudbase/functions-typings'
 
+import { x } from './lib'
+
 // GET no boyd
 export const main: TcbEventFunction = function(event, context) {
+  console.log('x', x)
   console.log('hello world')
   return 'b'
 }
